d8b3b76cb28334e1a510330b10a18a2cd2357d25
YOUR NAME <rodrigocortes98@live.com>
2018-08-12 20:26:42 -0400

    worked on todo app

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,3 +1,15 @@
+const notes = [{
+  title: 'My next trip',
+  body: 'I would like to go to Spain'
+ },{
+  title: 'Habits to work on',
+  body: 'Exercise. Eating a bit better.'
+ },{
+  title: 'Office modification',
+  body: 'Get a new seat'
+ }
+]
+
 const ps = document.querySelectorAll('p')
 
 ps.forEach(function (p) {
