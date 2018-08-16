6a94fee5d0dd4b121908416e322342790043040c
YOUR NAME <rodrigocortes98@live.com>
2018-08-15 23:02:18 -0400

    shows todos in browser

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,14 +1,38 @@
-const notes = [{
-  title: 'My next trip',
-  body: 'I would like to go to Spain'
- },{
-  title: 'Habits to work on',
-  body: 'Exercise. Eating a bit better.'
- },{
-  title: 'Office modification',
-  body: 'Get a new seat'
- }
+const toDos = [
+  {
+    text: 'Buy a house',
+    completed: true
+  },
+  {
+    text: 'Buy a car',
+    completed: true
+  },
+  {
+    text: 'Job?',
+    completed: true
+  },
+  {
+    text: 'Study',
+    completed: false
+  },
+  {
+    text: 'Shop',
+    completed: false
+  }
 ]
 
 // You have 2 toDos left (p element)
+const toDosLeft = toDos.filter(function (toDo) {
+  return !toDo.completed
+})
+
+const message = document.createElement('h2')
+message.textContent = `You have ${toDosLeft.length} toDos left`
+document.querySelector('body').appendChild(message)
+
 // add a p for each todo above (use text value)
+toDosLeft.forEach(function (toDo) {
+  const toDoShown = document.createElement('h3')
+  toDoShown.textContent = toDo.text
+  document.querySelector('body').appendChild(toDoShown)
+})
