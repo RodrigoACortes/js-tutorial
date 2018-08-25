8a593ddc63870abf9665c20dc2bcde5d14a95e83
YOUR NAME <rodrigocortes98@live.com>
2018-08-24 21:42:18 -0400

    write new notes to local storage

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,31 +1,14 @@
-const notes = [{
-  title: 'My next trip',
-  body: 'I would like to go to Spain'
- },{
-  title: 'Habits to work on',
-  body: 'Exercise. Eating a bit better.'
- },{
-  title: 'Office modification',
-  body: 'Get a new seat'
- }
-]
+let notes = []
 
 const filters = {
   searchText: ''
 }
 
-// const user = {
-//   name: 'Me',
-//   age: 100
-// }
+const notesJSON = localStorage.getItem('notes')
 
-// const userJSON = JSON.stringify(user)
-// console.log(userJSON)
-// localStorage.setItem('user', userJSON)
-
-const userJSON = localStorage.getItem('user')
-const user = JSON.parse(userJSON)
-console.log(`${user.name} is ${user.age}`)
+if (notesJSON !== null) {
+  notes = JSON.parse(notesJSON)
+}
 
 const renderNotes = function (notes, filters) {
 
@@ -37,7 +20,12 @@ const renderNotes = function (notes, filters) {
 
   filteredNotes.forEach(function (note) {
     const noteEl = document.createElement('p')
-    noteEl.textContent = note.title
+
+    if (note.title.length > 0) {
+      noteEl.textContent = note.title
+    } else {
+      noteEl.textContent = 'Unnamed note'
+    }
     document.querySelector('#notes').appendChild(noteEl)
   })
 }
@@ -45,7 +33,12 @@ const renderNotes = function (notes, filters) {
 renderNotes(notes, filters)
 
 document.querySelector('#create-note').addEventListener('click', function (e) {
-  e.target.textContent = 'The Button was clicked!'
+  notes.push({
+    title: '',
+    body: ''
+  })
+  localStorage.setItem('notes', JSON.stringify(notes))
+  renderNotes(notes, filters)
 })
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
