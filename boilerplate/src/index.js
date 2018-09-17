f879f781afcb69f368c64ba274077dab8aa52de2
YOUR NAME <rodrigocortes98@live.com>
2018-09-16 20:30:57 -0400

    exploring rest parameters

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,5 +1,8 @@
-const calculateAverage = (num1, num2) => {
-  return (num1 + num2) / 2
+const calculateAverage = (...numbers) => {
+  // return (num1 + num2) / 2
+  let sum = 0
+  numbers.forEach(num => sum += num)
+  return sum / numbers.length
 }
 
-console.log(calculateAverage(0, 100))
+console.log(calculateAverage(0, 100, 88, 64))
